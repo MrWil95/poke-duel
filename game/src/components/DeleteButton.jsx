e552f41f204e99fc12c04d331eae8e328ade997d
MrWil95 <86026932+MrWil95@users.noreply.github.com>
2021-09-20 10:27:54 -0500

    Update DeleteButton.jsx

--- game/src/components/DeleteButton.jsx
+++ game/src/components/DeleteButton.jsx
@@ -1,14 +1,14 @@
-import { useHistory } from 'react-router'
+import { useHistory } from 'react-router-dom'
 import { deletePokémon } from '../services'
 
 export default function DeleteButton(props) {
    const history = useHistory()
    const handleDelete = async () => {
       await deletePokémon(props.id)
-      history.push('/pokemonlists')
+      history.go(0)
    }
 
    return (
-      <button conClick={handleDelete}>Delete</button>
+      <button onClick={handleDelete}>Delete</button>
    )
 }
